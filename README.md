7358cd199f5f656f7ee2b3015a95b31e2236b867
Eric MORAND <eric.morand@gmail.com>
2023-10-09 02:26:55 +0200

    Resolve issue #15

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 
 Rebase your HTML assets relatively to the source file they were imported from.
 
+## Installation
+
+```bash
+npm install html-source-map-rebase
+```
+
 ## Example
 
 Consider the following Twig sources:
@@ -34,93 +40,36 @@ By rebasing the assets relatively to the file they were imported from, the resul
 html-source-map-rebase uses the mapping provided by source maps to resolve the original file the assets where imported from. That's why it *needs* a source map to perform its magic. Any tool able to generate a source map from a source file is appropriate. Here is how one could use [Twing](https://www.npmjs.com/package/twing) and html-source-map-rebase together to render an HTML document and rebase its assets.
 
 ``` javascript
-const {TwingEnvironment, TwingLoaderFilesystem} = require('twing');
-const Readable = require('stream').Readable;
-const through = require('through2');
-const Rebaser = require('html-source-map-rebase');
+import {TwingEnvironment, TwingLoaderFilesystem} from "twing";
+import {createRebaser} from "html-source-map-rebase";
 
-let loader = new TwingLoaderFilesystem('src');
-let twing = new TwingEnvironment(loader, {
+const loader = new TwingLoaderFilesystem('src');
+const environment = new TwingEnvironment(loader, {
   source_map: true
 });
 
-let html = twing.render('index.twig');
-
-let rebaser = new Rebaser({
-  map: twing.getSourceMap()
-});
-
-let data = '';
-let stream = new Readable({
-    encoding: 'utf8'
-});
-
-stream
-  .pipe(rebaser)
-  .pipe(through(function (chunk, enc, cb) {
-    data += chunk;
-
-    cb();
-  }))
-  .on('finish', function () {
-    console.log(data); // data contains the rendered HTML with rebased assets
-  })
-;
-
-stream.push(html);
-stream.push(null);
+return environment.render('index.twig')
+    .then((html) => {
+        const map = environment.getSourceMap();
+        const rebaser = createRebaser(Buffer.from(map));
+    
+        return rebaser.rebase(html);
+    })
+    .then((result) => {
+        // result.data contains the HTML markup with rebased assets
+        // result.map contains the source map
+    });
 ```
 
 ## API
 
-`let Rebaser = require('html-source-map-rebase')`
-
-### rebaser = new Rebaser(opts={})
-
-Return an object transform stream `rebaser` that expects entry filenames.
-
-Optionally pass in some opts:
-
-* opts.map:
-  
-    The belonging source map in the form of a JSON string. Defaults to `null`. Note that this module basically does nothing without a source map.
-
-* opts.rebase:
-    
-    Handles when the rebaser encounters an asset that may need rebasing.
-  
-    * Type: `Function`
-    * Default: `undefined`
-    * Signature:
-        * `url (Url)` - the [Url](https://nodejs.org/api/url.html) of the asset that may need rebasing.
-        * `source (String)` - the path of the file the asset was imported from.
-        * `done (Function)` - a callback function to invoke on completion. Accepts either `false`, `null`, `undefined` or an [Url](https://nodejs.org/api/url.html) as parameter. When called with `false`, the asset will not be rebased. When called with either `null` or `undefined`, the asset will be rebased using the [default rebasing logic](#rebasing-logic). When called with an Url, the asset will be rebased to that Url.
-
-## <a name="rebasing-logic"></a>Rebasing logic
-
-When html-source-map-rebase encounters an asset that may need rebasing, it first checks if it is a remote or a local asset. In the former case, the asset is not rebased at all. In the latter case, the asset is rebased be appending the asset path to the path of the source it's coming from.
-
-For example, a `foo/bar.png` asset coming from `/lorem/ipsum/index.twig` would be rebased to `/lorem/ipsum/foo/bar.png`.
-
-## Events
-
-In addition to the usual events emitted by node.js streams, html-source-map-rebase emits the following events:
-
-### rebaser.on('rebase', function(url) {})
-
-Every time an asset is rebased, this event fires with the rebased [Url](https://nodejs.org/api/url.html).
-
-## Installation
-
-```bash
-npm install html-source-map-rebase
-```
+Read the [documentation](https://nightlycommit.github.io/html-source-map-rebase) for more information.
 
 ## Contributing
 
 * Fork the main repository
 * Code
-* Implement tests using [node-tap](https://github.com/tapjs/node-tap)
+* Implement tests using [tape](https://www.npmjs.com/package/tape)
 * Issue a pull request keeping in mind that all pull requests must reference an issue in the issue queue
 
 ## License
---
+++ src/lib/Rebaser.ts
@@ -0,0 +1,248 @@
+import RewritingStream from "parse5-html-rewriting-stream";
+import {SourceMapConsumer} from "source-map";
+import type {StartTag} from "parse5-sax-parser";
+import {EventEmitter} from "events";
+import {parse, Url} from "url";
+import {posix, isAbsolute, dirname, join} from "path";
+import slash from "slash"
+import {Readable, Writable} from "stream"
+
+export type Result = {
+  data: Buffer,
+  map: Buffer
+};
+
+/**
+ * @param rebasedPath The rebased path of the asset. When `false`, the asset will not be rebased. When either `null` or `undefined`, the asset will be rebased using the default rebasing logic. When a `string`, the asset will be rebased to that string.
+ */
+export type RebaseHandlerCallback = (rebasedPath?: false | null | string) => void;
+
+/**
+ * @param source The source file where the asset was encountered.
+ * @param resolvedPath The resolved path of the asset - i.e. the path of the asset relative to the source file.
+ * @param done The callback function to invoke on completion.
+ */
+export type RebaseHandler = (source: string, resolvedPath: string, done: RebaseHandlerCallback) => void;
+
+export type Options = {
+  /**
+   * The handler invoked to resolve the rebased path of the asset. Takes precedence over the default rebasing logic.
+   */
+  rebase?: RebaseHandler
+};
+
+type Region = {
+  source: string;
+  startLine: number;
+  startColumn: number;
+  endLine: number | null;
+  endColumn: number | null;
+};
+
+/**
+ * @param rebasedPath The rebased path of the rebased asset
+ * @param resolvedPath The original path of the rebased asset
+ */
+export type RebaseEventListener = (rebasedPath: string, resolvedPath: string) => void;
+
+export interface Rebaser {
+  /**
+   * Rebases the passed HTML markup assets reference.
+   *
+   * @param html The HTML markup whose assets need to be rebased.
+   */
+  rebase: (html: Buffer) => Promise<Result>;
+
+  /**
+   * Adds the listener to the end of the listeners array for the event "rebase".
+   *
+   * @param event
+   * @param listener
+   */
+  on(event: "rebase", listener: RebaseEventListener): this;
+}
+
+/**
+ * Creates and returns a Rebaser instance.
+ *
+ * @param map The source map that will be used to resolve the assets.
+ * @param options
+ */
+export const createRebaser = (
+  map: Buffer,
+  options: Options = {}
+): Rebaser => {
+  const eventEmitter = new EventEmitter();
+
+  const rebase: Rebaser["rebase"] = (
+    html
+  ) => {
+    const rewritingStream = new RewritingStream();
+    let {rebase} = options;
+    let regions: Array<Region> | null = null;
+
+    return new Promise((resolve, reject) => {
+      let data: Buffer = Buffer.from('');
+
+      const inputStream = new Readable({
+        encoding: "utf-8"
+      });
+
+      const outputStream = new Writable({
+        write(chunk: any, _encoding: BufferEncoding, callback: (error?: (Error | null)) => void) {
+          data = Buffer.concat([data, chunk]);
+
+          callback();
+        }
+      });
+
+      outputStream.on('finish', () => {
+        resolve({
+          data,
+          map
+        });
+      });
+
+      inputStream
+        .pipe(rewritingStream)
+        .pipe(outputStream);
+
+      const isRebasable = (url: Url): boolean => {
+        return !isAbsolute(url.href) && (url.host === null) && ((url.hash === null) || (url.path !== null));
+      };
+
+      const getRegions = () => {
+        if (!regions) {
+          const foundRegions: Array<Region> = [];
+
+          let sourceMapConsumer = new SourceMapConsumer(JSON.parse(map.toString()));
+
+          let region: Region | null = null;
+          let currentSource: string | null = null;
+
+          sourceMapConsumer.eachMapping((mapping) => {
+            let source = mapping.source;
+
+            if (source !== currentSource) {
+              // end the current region...
+              if (region) {
+                region.endLine = mapping.generatedLine;
+                region.endColumn = mapping.generatedColumn;
+              }
+
+              //...and start a new one
+              region = {
+                source: source,
+                startLine: mapping.generatedLine,
+                startColumn: mapping.generatedColumn,
+                endLine: null,
+                endColumn: null
+              };
+
+              foundRegions.push(region);
+
+              currentSource = source;
+            }
+          }, null);
+
+          regions = foundRegions;
+        }
+
+        return regions;
+      }
+
+      const transformStartTag = (tag: StartTag) => {
+        const processTag = (tag: StartTag) => {
+          const attributes = tag.attrs;
+
+          attributes.forEach((attribute) => {
+            switch (attribute.name) {
+              case 'href':
+              case 'src':
+                const url = parse(attribute.value);
+
+                if (isRebasable(url)) {
+                  const location = tag.sourceCodeLocation!;
+
+                  let tagStartLine = location.startLine;
+                  let tagStartColumn = location.startCol - 1;
+
+                  let i = 0;
+                  let tagRegion: Region | null = null;
+                  let regions = getRegions();
+
+                  while ((i < regions.length) && (tagRegion === null)) {
+                    let region = regions[i];
+
+                    if (
+                      ((region.startLine < tagStartLine) || ((region.startLine === tagStartLine) && (region.startColumn <= tagStartColumn))) &&
+                      (
+                        (region.endLine === null) || (region.endLine > tagStartLine) ||
+                        ((region.endLine === tagStartLine) && (region.endColumn === null || (region.endColumn >= tagStartColumn)))
+                      )
+                    ) {
+                      tagRegion = region;
+                    }
+
+                    i++;
+                  }
+
+                  const {source} = tagRegion!;
+
+                  const resolvedPath = posix.join(dirname(source), url.pathname!);
+
+                  const done: RebaseHandlerCallback = (rebasedPath) => {
+                    if (rebasedPath !== false) {
+                      if (!rebasedPath) { // default rebasing
+                        rebasedPath = resolvedPath;
+                      }
+
+                      attribute.value = slash(join('.', rebasedPath))!;
+
+                      eventEmitter.emit('rebase', rebasedPath, resolvedPath);
+                    }
+                  };
+
+                  if (!rebase) {
+                    rebase = (_url, _source, done) => {
+                      done();
+                    };
+                  }
+
+                  rebase(source, resolvedPath, done);
+                }
+
+                break;
+            }
+          });
+        };
+
+        processTag(tag);
+      }
+
+      rewritingStream.on('startTag', (startTag) => {
+        try {
+          transformStartTag(startTag);
+
+          rewritingStream.emitStartTag(startTag);
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+      inputStream.push(html);
+      inputStream.push(null);
+    });
+  };
+
+  const rebaser: Rebaser = {
+    rebase,
+    on(event, listener) {
+      eventEmitter.on(event, listener);
+
+      return rebaser;
+    }
+  };
+
+  return rebaser;
+}
